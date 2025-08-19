4c5a4f01b10be89c84419d1faec88b317ff2919f
Birate Arthur <biratearthur@Birates-MacBook-Air.local>
2025-08-19 17:45:36 +0200

    v2.9.19.22

--- projects/views.py
+++ projects/views.py
@@ -45,3 +45,52 @@ def edit_project(request, pk):
 def project_detail(request, pk):
     project = get_object_or_404(Project, pk=pk)
     return render(request, 'project.html', {'project': project})
+
+
+from django.shortcuts import render, get_object_or_404
+from .models import Project
+
+# Project 1 - YOLOv5/YOLOv8
+def project1_detail(request):
+    project = get_object_or_404(Project, id=1)
+    return render(request, "projects/waldo.html", {
+        "image1": project.image1,
+        "image2": project.image2,
+        "project": project,  # in case you want links too
+    })
+
+# Project 2 - HR Analytics Dashboard
+def project2_detail(request):
+    project = get_object_or_404(Project, id=2)
+    return render(request, "projects/hr.html", {
+        "image1": project.image1,
+        "image2": project.image2,
+        "project": project,
+    })
+
+# Project 3 - VisiAge
+def project3_detail(request):
+    project = get_object_or_404(Project, id=3)
+    return render(request, "projects/visiAge.html", {
+        "image1": project.image1,
+        "image2": project.image2,
+        "project": project,
+    })
+
+# Project 4 - Olympics Dashboard
+def project4_detail(request):
+    project = get_object_or_404(Project, id=4)
+    return render(request, "projects/olympic.html", {
+        "image1": project.image1,
+        "image2": project.image2,
+        "project": project,
+    })
+
+# Project 5 - Superstore Analytics
+def project5_detail(request):
+    project = get_object_or_404(Project, id=5)
+    return render(request, "projects/superstore.html", {
+        "image1": project.image1,
+        "image2": project.image2,
+        "project": project,
+    })
